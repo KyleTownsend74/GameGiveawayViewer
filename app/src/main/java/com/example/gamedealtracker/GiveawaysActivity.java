cb7d036017c473cfd91cf0adb844272304ce96df
KyleTownsend74 <townsend.kyle.kt@gmail.com>
2021-10-12 22:30:29 -0700

    Add giveaways recycler view
    Add functional basic recycler view for giveaway list
    Add image loading for recycler view with Glide library

--- app/src/main/java/com/example/gamedealtracker/GiveawaysActivity.java
+++ app/src/main/java/com/example/gamedealtracker/GiveawaysActivity.java
@@ -1,14 +1,34 @@
 package com.example.gamedealtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.gamedealtracker.adapters.GiveawaysAdapter;
+import com.example.gamedealtracker.models.Giveaway;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GiveawaysActivity extends AppCompatActivity {
+
+    private List<Giveaway> giveaways;
+    private RecyclerView rvGiveaways;
+    private GiveawaysAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giveaways);
+
+        rvGiveaways = findViewById(R.id.rvGiveaways);
+
+        giveaways = new ArrayList<>();
+        adapter = new GiveawaysAdapter(this, giveaways);
+
+        rvGiveaways.setAdapter(adapter);
+        rvGiveaways.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/gamedealtracker/adapters/GiveawaysAdapter.java
@@ -0,0 +1,64 @@
+package com.example.gamedealtracker.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.gamedealtracker.R;
+import com.example.gamedealtracker.models.Giveaway;
+
+import java.util.List;
+
+public class GiveawaysAdapter extends RecyclerView.Adapter<GiveawaysAdapter.ViewHolder> {
+
+    Context context;
+    List<Giveaway> giveaways;
+
+    public GiveawaysAdapter(Context context, List<Giveaway> giveaways) {
+        this.context = context;
+        this.giveaways = giveaways;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_giveaway, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Giveaway giveaway = giveaways.get(position);
+        holder.bind(giveaway);
+    }
+
+    @Override
+    public int getItemCount() {
+        return giveaways.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView ivGiveawayItem;
+        TextView tvGiveawayItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ivGiveawayItem = itemView.findViewById(R.id.ivGiveawayItem);
+            tvGiveawayItem = itemView.findViewById(R.id.tvGiveawayItem);
+        }
+
+        public void bind(Giveaway giveaway) {
+            Glide.with(context).load(giveaway.getThumbnailUrl()).into(ivGiveawayItem);
+            tvGiveawayItem.setText(giveaway.getTitle());
+        }
+    }
+}
