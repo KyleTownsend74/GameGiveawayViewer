20b7756bf8e0d02341d538e73e5efa36190d7045
KyleTownsend74 <townsend.kyle.kt@gmail.com>
2021-10-12 21:33:50 -0700

    Create basic giveaway model with layout

---
+++ app/src/main/java/com/example/gamedealtracker/models/Giveaway.java
@@ -0,0 +1,20 @@
+package com.example.gamedealtracker.models;
+
+public class Giveaway {
+
+    private String title;
+    private String description;
+    private String thumbnailUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+}
